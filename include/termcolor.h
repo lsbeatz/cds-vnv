70f5fbdbca857a8b9e082f74c95bfc825517a86f
lsbeatz <dlatjdqo34@gmail.com>
2024-09-14 22:57:05 +0900

    feat: add termcolor.h for colored log

---
+++ include/termcolor.h
@@ -0,0 +1,12 @@
+#ifndef TERMCOLOR_H
+#define TERMCOLOR_H
+
+#define TERMCOLOR_RED	  "\33[31m"
+#define TERMCOLOR_GREEN	  "\33[32m"
+#define TERMCOLOR_YELLOW  "\33[33m"
+#define TERMCOLOR_BLUE	  "\33[34m"
+#define TERMCOLOR_MAGENTA "\33[35m"
+#define TERMCOLOR_CYAN	  "\33[36m"
+#define TERMCOLOR_RESET	  "\33[0m"
+
+#endif /* TERMCOLOR_H */
