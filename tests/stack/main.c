365e0aeb5299212a68c94cf56bc79aba6dfcfa12
lsbeatz <dlatjdqo34@gmail.com>
2024-09-17 16:04:15 +0900

    feat: add test cases for stack

--- tests/stack/main.c
+++ tests/stack/main.c
@@ -1,3 +1,5 @@
+#include <stdlib.h>
+#include <errno.h>
 #include <common.h>
 #include <sizes.h>
 #include <testsuite.h>
@@ -40,6 +42,64 @@ TEST_CASE(stack, push_and_pop)
 		TEST_ASSERT_EQ(stack_peek(s), items[i]);
 		rc = stack_pop(s);
 		TEST_ASSERT_EQ(rc, 0);
+	}
+
+	stack_destroy(s);
+
+	return TEST_PASS;
+}
+
+TEST_CASE(stack, grow)
+{
+	struct stack *s;
+	int rc;
+	size_t size = (SZ_64 / sizeof(int));
+	size_t max_size = (SZ_4K / sizeof(int));
+
+	s = stack_create();
+
+	for (int i = 0; i < max_size + 1; i++) {
+		rc = stack_push(s, i);
+		if (i >= max_size) {
+			TEST_ASSERT_EQ(rc, -ENOMEM);
+			TEST_ASSERT_EQ(stack_capacity(s), max_size);
+			TEST_ASSERT_EQ(stack_peek(s), max_size - 1);
+		} else if (i >= size) {
+			size = (size << 1);
+			TEST_ASSERT_EQ(rc, 0);
+			TEST_ASSERT_EQ(stack_capacity(s), size);
+			TEST_ASSERT_EQ(stack_peek(s), i);
+		} else {
+			TEST_ASSERT_EQ(rc, 0);
+			TEST_ASSERT_EQ(stack_capacity(s), size);
+			TEST_ASSERT_EQ(stack_peek(s), i);
+		}
+	}
+
+	for (int i = max_size - 1; i >= 0; i--) {
+		TEST_ASSERT_EQ(stack_peek(s), i);
+		rc = stack_pop(s);
+		TEST_ASSERT_EQ(rc, 0);
+		TEST_ASSERT_EQ(stack_capacity(s), max_size);
+	}
+	TEST_ASSERT_EQ(stack_peek(s), -EINVAL);
+
+	stack_destroy(s);
+
+	return TEST_PASS;
+}
+
+TEST_CASE(stack, over_pop)
+{
+	struct stack *s;
+	int rc;
+
+	s = stack_create();
+
+	for (int i = 0; i < 10; i++) {
+		rc = stack_pop(s);
+		TEST_ASSERT_EQ(rc, -EINVAL);
+		TEST_ASSERT_EQ(stack_size(s), 0);
 	}
 
 	stack_destroy(s);
